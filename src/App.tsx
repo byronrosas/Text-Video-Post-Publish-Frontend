608d7216f34f134e270d7985bbfba098e708b0db
Byron Rosas <byron_mma@hotmail.es>
2020-01-17 00:17:54 -0500

    structure redux, navbar, router

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import Navbar from 'react-bootstrap/Navbar';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import Nav from 'react-bootstrap/Nav';
+import NavItem from 'react-bootstrap/NavItem';
+import Button from 'react-bootstrap/Button';
+
+import { LinkContainer } from 'react-router-bootstrap';
+import AdminPage from './pages/admin-page';
+import FeedPage from './pages/feed-page';
+import FullReadPostPage from './pages/full-read-post-page';
+
 import './App.css';
+
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="mr-auto">
+          <LinkContainer to="/feed">
+            <NavItem><Button style={{ marginRight: '12px' }} variant="outline-light">Feed</Button></NavItem>
+          </LinkContainer>                      
+          <LinkContainer to="/admin">
+            <NavItem><Button style={{ marginRight: '12px' }} variant="outline-light">Admin</Button></NavItem>
+          </LinkContainer>                    
+        </Nav>
+      </Navbar>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Switch>
+            <Redirect exact from="/" to="/feed" />
+            <Route path="/feed" component={FeedPage} />
+            <Route path="/admin" component={AdminPage} />
+            <Route path="/fullread/:id" component={FullReadPostPage} />
+          </Switch>
+        </BrowserRouter>
+      </Provider>
+    </>
   );
 }
 
---
+++ src/redux/store.tsx
@@ -0,0 +1,10 @@
+import { createStore, combineReducers } from 'redux';
+// import textReducer from './reducers/textReducer';
+
+const reducer = combineReducers({
+
+});
+
+const store = createStore( reducer );
+
+export default store;
